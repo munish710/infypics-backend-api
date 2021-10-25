29479c2b7d6eaff08d599fe7305d3c241c109070
munish710 <munish710@gmail.com>
2021-10-25 12:32:42 +0530

    feat : connect to db, intitalize connection

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const connectDB = require("./db/db.connect");
+
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
@@ -11,6 +15,15 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URI);
+    app.listen(port, () => {
+      console.log(`Server listening on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
