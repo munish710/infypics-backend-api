6c1a35861032566871e0754a7f224a7d7de186b4
munish710 <munish710@gmail.com>
2021-10-25 16:23:23 +0530

    feat : controllers - add createPost

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -5,7 +5,25 @@ const getAllPosts = (req, res) => {
 };
 
 const createPost = async (req, res) => {
-  console.log("Create a Post");
+  try {
+    const foundPost = await Post.findOne({
+      userEmail: req.body.userEmail,
+      imageID: req.body.imageID,
+    });
+
+    if (!foundPost) {
+      await Post.create(req.body);
+      res
+        .status(200)
+        .json({ success: true, message: "Image Saved Successfully" });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Image Already Saved" });
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "server error" });
+  }
 };
 
 const deletePost = (req, res) => {
